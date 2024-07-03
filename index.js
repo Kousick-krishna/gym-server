f6c92353b76dd25f1263e261d4b8d187284c57ad
Kousick-krishna <kousickkrish1970@gmail.com>
2024-07-03 07:12:35 +0530

    changes

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin : ["https://gym-client-wheat.vercel.app"],
+    methods : ["GET","POST","PUT","DELETE"],
+    credentials:true
+}));
 
 const mongoDBURL = process.env.MONGODB_URL;
 
